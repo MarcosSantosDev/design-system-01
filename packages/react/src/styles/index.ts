9e576ad37a9da7e49952bbf328e5a60433b559ba
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-10-23 23:08:17 -0300

    feat: first storybook component

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -1,6 +1,14 @@
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
-import tokens from '@ignite-ui/tokens'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
 
 export const {
   styled,
@@ -18,12 +26,12 @@ export const {
     width: 'space',
   },
   theme: {
-    colors: tokens.colors,
-    fontSizes: tokens.fontSizes,
-    fontWeights: tokens.fontWeights,
-    fonts: tokens.fonts,
-    lineHeights: tokens.lineHeights,
-    radii: tokens.radii,
-    space: tokens.space,
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
   },
 })
