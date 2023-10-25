12705e0b668acef99ab04087c0be3de719f12b0b
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-10-24 22:54:43 -0300

    feat: storybook load fonts

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -27,5 +27,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  previewHead: head => `
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+  `
 };
 export default config;
