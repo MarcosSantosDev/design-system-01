6fdbb4bfb4a8d1348da351b85b89c8cf115f00be
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-11-04 20:45:17 -0300

    feat: button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,50 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Button, ButtonProps } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+
+export default {
+  title: "Form/Button",
+  component: Button,
+  args: {
+    children: 'Label',
+    disabled: false
+  },
+  argTypes: {
+    disabled: {
+      type: 'boolean'
+    }
+  }
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new'
+  }
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel'
+  }
+};
+
+export const SmallSize: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  }
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next Step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  }
+};
