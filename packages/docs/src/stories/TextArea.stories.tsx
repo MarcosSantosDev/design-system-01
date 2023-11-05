32292d18bf130dca44170095ad15ff9fe41e0612
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-11-04 21:50:53 -0300

    feat: text area component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Box, Text, TextArea, TextAreaProps } from "@ignite-ui/react";
+
+export default {
+  title: "Form/Text Area",
+  component: TextArea,
+  args: {
+    disabled: false
+  },
+  argTypes: {
+    disabled: {
+      type: 'boolean'
+    }
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size='sm'>Observations</Text>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<TextAreaProps>;
+
+export const Primary = {
+  args: {
+    placeholder: 'Type any observations...'
+  }
+} as StoryObj<TextAreaProps>;
