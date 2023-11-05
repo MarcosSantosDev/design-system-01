91d93810baf30e697b92afa792318305f762273e
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-11-05 13:39:32 -0300

    feat: checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Box, Text, Checkbox, CheckboxProps } from "@ignite-ui/react";
+
+export default {
+  title: "Form/Checkbox",
+  component: Checkbox,
+  args: {
+    disabled: false
+  },
+  argTypes: {
+    disabled: {
+      type: 'boolean'
+    }
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size='sm'>Accept terms of use</Text>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<CheckboxProps>;
+
+export const Primary = {} as StoryObj<CheckboxProps>;
