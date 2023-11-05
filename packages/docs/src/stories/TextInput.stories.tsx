0a0eae7656127d625b576e67cd885fe697b0a972
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-11-04 21:39:31 -0300

    feat: text input component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Box, Text, TextInput, TextInputProps } from "@ignite-ui/react";
+
+export default {
+  title: "Form/Text Input",
+  component: TextInput,
+  args: {
+    disabled: false
+  },
+  argTypes: {
+    disabled: {
+      type: 'boolean'
+    }
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size='sm'>User</Text>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<TextInputProps>;
+
+export const Primary = {
+  args: {
+    placeholder: 'Type your user name'
+  }
+} as StoryObj<TextInputProps>;
+
+export const WithPrefix = {
+  args: {
+    prefix: 'github.com/'
+  }
+} as StoryObj<TextInputProps>;
