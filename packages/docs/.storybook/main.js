b1bc58dbbe2b5d534e9887f98656db58f8ab76a9
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-11-06 15:22:46 -0300

    fix: add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -33,11 +33,6 @@ const config = {
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-  `,
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/design-system-01/'
-    }
-  }
+  `
 };
 export default config;
